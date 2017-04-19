aabbfe25024f56c0e91458e3f5694e35e8e3b366
Ikram Ali <mrikram1989@gmail.com>
2017-04-19 09:17:02 +0500

    [setup] classifier updated

--- setup.py
+++ setup.py
@@ -43,9 +43,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
