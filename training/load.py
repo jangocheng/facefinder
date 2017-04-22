d93116beee4cf1a3595245e2998700379d32850c
Ikram Ali <mrikram1989@gmail.com>
2017-04-22 13:59:27 +0500

    [data] new lists_images function added

--- training/load.py
+++ training/load.py
@@ -5,7 +5,7 @@ import os
 import config
 from skimage import io
 from skimage.transform import resize, rotate
-
+from helper.path import list_images
 from sklearn.model_selection import train_test_split
 
 
@@ -17,41 +17,27 @@ def load_dataset():
      Null
     """
 
-    # Load faces (faces samples)
     image_numpy_data = []
     image_vector_data = []
-    for folder in os.listdir(config.FACES_FOLDER_DIR):
 
-        if os.path.isdir(os.path.join(config.FACES_FOLDER_DIR, folder)):
+    for image_path in list_images(config.FACES_FOLDER_DIR):
+        try:
+            img = resize(io.imread(image_path), config.IMAGE_DIMENSION)
+            image_numpy_data.append(img)
+            image_vector_data.append(config.FACES_VECTOR)
 
-            for image in os.listdir(os.path.join(config.FACES_FOLDER_DIR, folder)):
-                image_file = os.path.join(config.FACES_FOLDER_DIR, folder, image)
+        except IOError:
+            print("Image type not found {}".format(image_path))
 
-                if os.path.isfile(image_file):
-                    try:
-                        img = resize(io.imread(image_file), config.IMAGE_DIMENSION)
-                        image_numpy_data.append(img)
-                        image_vector_data.append(config.FACES_VECTOR)
+    for image_path in list_images(config.NON_FACES_FOLDER_DIR):
+        try:
+            img = resize(io.imread(image_path), config.IMAGE_DIMENSION)
+            image_numpy_data.append(img)
+            image_vector_data.append(config.NON_FACES_VECTOR)
 
-                    except IOError:
-                        print("Image type not found {}".format(image_file))
+        except IOError:
+            print("Image type not found {}".format(image_path))
 
-    # Load NONfaces (NONfaces samples)
-    for folder in os.listdir(config.NON_FACES_FOLDER_DIR):
-
-        if os.path.isdir(os.path.join(config.NON_FACES_FOLDER_DIR, folder)):
-
-            for image in os.listdir(os.path.join(config.NON_FACES_FOLDER_DIR, folder)):
-                image_file = os.path.join(config.NON_FACES_FOLDER_DIR, folder, image)
-
-                if os.path.isfile(image_file):
-                    try:
-                        img = resize(io.imread(image_file), config.IMAGE_DIMENSION)
-                        image_numpy_data.append(img)
-                        image_vector_data.append(config.NON_FACES_VECTOR)
-                    except IOError:
-                        print("Image type not found {}".format(image_file))
-
-    x_train, x_test, y_train, y_test = train_test_split(image_numpy_data, image_vector_data, test_size=0.4,
+    x_train, x_test, y_train, y_test = train_test_split(image_numpy_data, image_vector_data, test_size=0.7,
                                                         random_state=4)
     return x_train, x_test, y_train, y_test
