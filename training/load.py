790a2c764743e5340b8a5b2b000c58e3c632762b
Ikram Ali <mrikram1989@gmail.com>
2017-04-24 09:38:00 +0500

    [data] numpy flatten() function added

--- training/load.py
+++ training/load.py
@@ -2,9 +2,11 @@
 """Load images dataset"""
 from __future__ import division
 import os
+import numpy as np
 import config
 from skimage import io
 from skimage.transform import resize, rotate
+from skimage.color import rgb2gray
 from helper.path import list_images
 from sklearn.model_selection import train_test_split
 
@@ -23,7 +25,7 @@ def load_dataset():
     for image_path in list_images(config.FACES_FOLDER_DIR):
         try:
             img = resize(io.imread(image_path), config.IMAGE_DIMENSION)
-            image_numpy_data.append(img)
+            image_numpy_data.append(img.flatten().astype(np.float32))
             image_vector_data.append(config.FACES_VECTOR)
 
         except IOError:
@@ -32,11 +34,14 @@ def load_dataset():
     for image_path in list_images(config.NON_FACES_FOLDER_DIR):
         try:
             img = resize(io.imread(image_path), config.IMAGE_DIMENSION)
-            image_numpy_data.append(img)
+            image_numpy_data.append(img.flatten().astype(np.float32))
             image_vector_data.append(config.NON_FACES_VECTOR)
 
         except IOError:
             print("Image type not found {}".format(image_path))
+
+    image_numpy_data = np.asarray(image_numpy_data)
+    image_vector_data = np.asarray(image_vector_data)
 
     x_train, x_test, y_train, y_test = train_test_split(image_numpy_data, image_vector_data, test_size=0.7,
                                                         random_state=4)
